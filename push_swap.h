4a688cb769c3a9c11dd2ecf74de308ad0b996fbe
Shepardinio <mel-yand@student.42.fr>
2024-01-09 16:54:14 +0100

    rien fait

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-yand <mel-yand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 16:37:17 by mel-yand          #+#    #+#             */
+/*   Updated: 2024/01/09 16:42:31 by mel-yand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}t_list;
+
+#endif
