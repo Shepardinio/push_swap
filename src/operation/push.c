87f0f4b02a0b2221fad1ab580fa47419768bbb55
Shepardinio <mel-yand@student.42.fr>
2024-02-05 21:07:49 +0100

    finifini

--- src/operation/push.c
+++ src/operation/push.c
@@ -6,7 +6,7 @@
 /*   By: mel-yand <mel-yand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:04:59 by mel-yand          #+#    #+#             */
-/*   Updated: 2024/02/05 16:52:26 by mel-yand         ###   ########.fr       */
+/*   Updated: 2024/02/05 21:06:50 by mel-yand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	pa(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack	*tmp;
 
-	if (!stack_b)
+	if (!*stack_b)
 		return ;
 	tmp = *stack_b;
 	*stack_b = (*stack_b)->next;
--- srcb/operation/push.c
+++ srcb/operation/push.c
@@ -6,7 +6,7 @@
 /*   By: mel-yand <mel-yand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:04:59 by mel-yand          #+#    #+#             */
-/*   Updated: 2024/02/05 16:56:04 by mel-yand         ###   ########.fr       */
+/*   Updated: 2024/02/05 21:05:21 by mel-yand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	pa(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack	*tmp;
 
-	if (!stack_b)
+	if (!*stack_b)
 		return ;
 	tmp = *stack_b;
 	*stack_b = (*stack_b)->next;
